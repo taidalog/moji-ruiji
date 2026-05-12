56f22401c6c5dd84fb343a5560385e2965e93b31
taidalog <72747279+taidalog@users.noreply.github.com>
2026-05-13 01:33:20 +0900

    Add a button to calcutate similarity

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from src import string_metric as sm
 st.title("moji-ruiji")
 
 s1 = st.text_area("元テキスト", "ここにテキストを入力してください")
-s2 = st.text_area("入力テキスト", "ここにテキストを入力してください")
+s2 = st.text_area("入力テキスト", "ここにもテキストを入力してください！")
 
 jsim = sm.StringMetric_jaroSimilarity(s1, s2)
-st.metric(label="類似度", value=float(jsim))
+m = st.metric(label="類似度", value=float(jsim))
+
+if st.button("類似度を計算"):
+    jsim = sm.StringMetric_jaroSimilarity(s1, s2)
+    m.metric(label="類似度", value=float(jsim))
